af1e16523155c1c0ac0af0ab96d9f9dded215a13
ritzyrithik <52700622+ritzyrithik@users.noreply.github.com>
2019-07-11 15:22:27 +0530

    nefjrg

---
+++ sourab.c
@@ -0,0 +1,36 @@
+jnfjbfn jfnckn cxnkcncncv vdknb fkjb gfjkngf bnchjvb cjags chagbc shbc djv fdbf bf.kv cn xn,mcx bcvx
+CVCvds'
+vdsbvfdVFD 'bfDb
+vV"v d'bb
+ c cxvc vbvcn cx c
+
+vv
+x
+vxx
+v
+xvx
+v
+xv
+xvx
+
+vx
+vx
+vx
+vx
+vx
+vx
+vx
+vx
+v
+xv
+xv
+xv
+x
+vx
+vx
+v
+xv
+xv
+x
+vx
+v
